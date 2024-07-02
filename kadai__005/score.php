77a33ec81e9e2f0917727ba6398adff3570f0865
tatemayu <164420579+tatemayu@users.noreply.github.com>
2024-07-02 16:18:58 +0900

    Add files via upload

---
+++ kadai__005/score.php
@@ -0,0 +1,36 @@
+
+<!Doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    //　変数を作成し、値を代入する
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    // 合計点を出力する
+    $score_total = $score1 + $score2+ $score3 +$score4 +$score5 +$score6 +$score7+ $score8 +$score9+$score10;
+
+    // 平均点を出力する
+    $score_average = $score_total / 10;
+
+    echo $score_average;
+    ?>
+    </p>
+</body>
+
+</html>
